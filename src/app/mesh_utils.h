03dfd7aa0527197e282be76eef03816f92d1fd78
Diego Mazala @lcg <diegomazala@gmail.com>
2018-09-26 15:22:22 -0300

    create grid function

--- src/app/mesh_utils.h
+++ src/app/mesh_utils.h
@@ -17,6 +17,13 @@ OpenMesh::VPropHandleT<TriMesh::TexCoord2D> TexCoord;
 
 
 template <typename T>
+constexpr T ndc(T v, T min, T max) // normalize to [0, 1]
+{
+	return (v - min) / (max - min);
+}
+
+
+template <typename T>
 static std::string filename_append_before_extension(const std::string& filename_in, const T& append, const char separator = '_')
 {
 	auto ext_pos = filename_in.rfind('.', filename_in.length());
@@ -122,3 +129,63 @@ static void normalize_mesh(TriMesh& mesh)
     normalize_mesh(mesh, center, scale);
 }
 
+
+static void create_grid_mesh(TriMesh& mesh, uint32_t rows, uint32_t cols, const OpenMesh::Vec3f& up = {0, 1, 0},float width = 1.0f, float height = 1.0f)
+{
+    std::vector<TriMesh::VertexHandle> vhandle((rows + 1) * (cols + 1));
+    std::vector<TriMesh::VertexHandle> fhandle(3);
+
+    for (auto i = 0; i <= rows; ++i)
+    {
+        for (auto j = 0; j <= cols; ++j)
+        {
+            auto x = static_cast<float>(j) / cols * width;
+            auto z = static_cast<float>(i) / rows * height;
+            vhandle[i*(rows+1)+j] = mesh.add_vertex(TriMesh::Point(x, 0.0f, z));
+            //vhandle[i*(rows+1)+j] = mesh.add_vertex(TriMesh::Point(x, z * up[2], z * up[1]));
+            //std::cout << i*(rows+1)+j << std::endl;
+        }
+    }
+
+    rows += 1;
+    cols += 1;
+
+    for (auto i = 0; i < rows - 1; ++i)
+    {
+        for (auto j = 1; j < cols; ++j)
+        {
+            //std::cout << i * rows + j << ' ' << (i + 1) * rows + j - 1 << ' ' << i * rows + j - 1 << std::endl;
+            fhandle[0] = vhandle[i * rows + j];
+            fhandle[2] = vhandle[(i + 1) * rows + j - 1];
+            fhandle[1] = vhandle[i * rows + j - 1];
+            mesh.add_face(fhandle);
+            //std::cout << i * rows + j << ' ' << (i + 1) * rows + j << ' ' << (i + 1) * rows + j - 1 << std::endl;
+            fhandle[0] = vhandle[i * rows + j];
+            fhandle[2] = vhandle[(i + 1) * rows + j];
+            fhandle[1] = vhandle[(i + 1) * rows + j - 1];
+            mesh.add_face(fhandle);
+            //std::cout << std::endl;
+        }
+    }
+}
+
+
+template <typename decimal>
+static void mesh_to_vecs(TriMesh& mesh, 
+    std::shared_ptr<std::vector<decimal>> U, 
+    std::shared_ptr<std::vector<decimal>> V, 
+    std::shared_ptr<std::vector<decimal>> Z)
+{
+	for (auto vi = mesh.vertices_begin(); vi != mesh.vertices_end(); ++vi)
+	{
+		auto uv = mesh.texcoord2D(*vi);
+		auto point = mesh.point(*vi);
+
+		//if (point[2] > 0.5)
+		{
+			U->push_back(uv[0]);
+			V->push_back(uv[1]);
+			Z->push_back(point[2]);
+		}
+	}
+}
